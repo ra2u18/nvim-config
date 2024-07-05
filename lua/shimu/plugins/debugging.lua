77e99365c13c076102b0d33b797f59f17e8e412a
netlemon <netlemon2021@gmail.com>
2024-07-05 11:51:46 +0300

    feat: added better debugger + test

--- lua/shimu/plugins/debugging.lua
+++ lua/shimu/plugins/debugging.lua
@@ -5,73 +5,126 @@ return {
     'theHamsta/nvim-dap-virtual-text',
     'nvim-neotest/nvim-nio',
     'williamboman/mason.nvim',
+    {
+      'microsoft/vscode-js-debug',
+      build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+    },
+    {
+      'mxsdev/nvim-dap-vscode-js',
+      config = function()
+        require('dap-vscode-js').setup {
+          debugger_path = vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug',
+          adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+        }
+      end,
+    },
+    'Joakker/lua-json5',
   },
   config = function()
     local dap = require 'dap'
-    local ui = require 'dapui'
+    local dapui = require 'dapui'
 
-    ui.setup()
+    -- DAP UI setup
+    dapui.setup()
 
-    vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
-    vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
+    -- Define supported file types
+    local js_based_languages = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'vue' }
 
-    vim.keymap.set('n', '<space>?', function()
-      ui.eval(nil, { enter = true })
-    end)
-
-    vim.keymap.set('n', '<F1>', dap.continue)
-    vim.keymap.set('n', '<F2>', dap.step_into)
-    vim.keymap.set('n', '<F3>', dap.step_over)
-    vim.keymap.set('n', '<F4>', dap.step_out)
-    vim.keymap.set('n', '<F5>', dap.step_back)
-    vim.keymap.set('n', '<F12>', dap.restart)
-
-    -- Setting up TypeScript debugging configuration
-    dap.adapters.node2 = {
-      type = 'executable',
-      command = 'node',
-      args = { '/home/linuxadmin/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
-    }
-
-    dap.configurations.typescript = {
-      {
-        type = 'node2',
-        request = 'launch',
-        name = 'Debug local file',
-        program = '${file}',
-        cwd = vim.fn.getcwd(),
-        runtimeArgs = { '-r', 'ts-node/register' },
-        sourceMaps = true,
-        protocol = 'inspector',
-        console = 'integratedTerminal',
-        env = {
-          AWS_REGION = 'eu-central-1',
+    -- Configure DAP for JavaScript/TypeScript
+    for _, language in ipairs(js_based_languages) do
+      dap.configurations[language] = {
+        -- Node.js debug configuration
+        {
+          type = 'pwa-node',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+          sourceMaps = true,
+          resolveSourceMapLocations = {
+            '${workspaceFolder}/**',
+            '!**/node_modules/**',
+          },
+          outFiles = { '${workspaceFolder}/**/dist/**/*.js' },
         },
-        outFiles = { '${workspaceFolder}/dist/**/*.js' },
-      },
-    }
-
-    dap.listeners.before.attach.dapui_config = function()
-      ui.open()
+        -- Attach to running Node.js process
+        {
+          type = 'pwa-node',
+          request = 'attach',
+          name = 'Attach',
+          processId = require('dap.utils').pick_process,
+          cwd = '${workspaceFolder}',
+          sourceMaps = true,
+        },
+        -- Debug web applications (client-side)
+        {
+          type = 'pwa-chrome',
+          request = 'launch',
+          name = 'Launch Chrome',
+          url = function()
+            local co = coroutine.running()
+            return coroutine.create(function()
+              vim.ui.input({ prompt = 'Enter URL: ', default = 'http://localhost:3000' }, function(url)
+                if url and url ~= '' then
+                  coroutine.resume(co, url)
+                end
+              end)
+            end)
+          end,
+          webRoot = '${workspaceFolder}',
+          sourceMaps = true,
+          userDataDir = false,
+        },
+      }
     end
 
-    dap.listeners.before.launch.dapui_config = function()
-      ui.open()
+    -- Function to load launch.json configurations
+    local function load_launch_json()
+      local launch_json_path = vim.fn.findfile('.vscode/launch.json', '.;')
+      if launch_json_path ~= '' then
+        require('dap.ext.vscode').load_launchjs(launch_json_path, {
+          ['pwa-node'] = js_based_languages,
+          ['node'] = js_based_languages,
+          ['chrome'] = js_based_languages,
+          ['pwa-chrome'] = js_based_languages,
+        })
+      end
     end
 
-    dap.listeners.before.event_terminated.dapui_config = function()
-      ui.close()
+    -- Load launch.json before starting debugging
+    dap.listeners.before.launch.load_launch_json = load_launch_json
+
+    -- Keymappings
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Continue' })
+    vim.keymap.set('n', '<leader>dsi', dap.step_into, { desc = 'Step Into' })
+    vim.keymap.set('n', '<leader>dso', dap.step_over, { desc = 'Step Over' })
+    vim.keymap.set('n', '<leader>dsu', dap.step_out, { desc = 'Step Out' })
+    vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = 'Open REPL' })
+    vim.keymap.set('n', '<leader>dl', dap.run_last, { desc = 'Run Last' })
+    vim.keymap.set('n', '<leader>dui', dapui.toggle, { desc = 'Toggle UI' })
+    vim.keymap.set('n', '<leader>dro', function()
+      dap.repl.open()
+      dapui.toggle()
+    end, { desc = 'Reopen UI' })
+
+    -- Auto open/close DAP UI
+    dap.listeners.after.event_initialized['dapui_config'] = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated['dapui_config'] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited['dapui_config'] = function()
+      dapui.close()
     end
 
-    dap.listeners.before.event_exited.dapui_config = function()
-      ui.close()
-    end
-
+    -- Function to terminate DAP session and close UI
     local function terminate_and_close_dap()
       dap.terminate()
-      ui.close()
+      dapui.close()
     end
 
-    vim.keymap.set('n', '<leader>dq', terminate_and_close_dap)
+    vim.keymap.set('n', '<leader>dq', terminate_and_close_dap, { desc = 'Terminate and Close' })
   end,
 }
--- lua/shimu/plugins/testing.lua
+++ lua/shimu/plugins/testing.lua
@@ -1,10 +1,79 @@
 return {
   'nvim-neotest/neotest',
-  optional = true,
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-neotest/neotest-jest',
+  },
+  config = function()
+    local neotest = require 'neotest'
+    local neotest_jest = require 'neotest-jest'
+
+    neotest.setup {
+      adapters = {
+        neotest_jest {
+          jestCommand = function(file)
+            local package_root = string.match(file, '(.-/packages/[^/]+/)')
+            if package_root then
+              local package_json_path = package_root .. 'package.json'
+              if vim.fn.filereadable(package_json_path) == 1 then
+                local package_json_content = vim.fn.readfile(package_json_path)
+                local package_json = vim.fn.json_decode(table.concat(package_json_content, '\n'))
+                if package_json and package_json.scripts and package_json.scripts.test then
+                  return 'pnpm --filter ' .. vim.fn.fnamemodify(package_root, ':h:t') .. ' test --'
+                end
+              end
+            end
+            return 'pnpm test --'
+          end,
+          jestConfigFile = function(file)
+            local package_root = string.match(file, '(.-/packages/[^/]+/)')
+            if package_root then
+              local config_path = package_root .. 'jest.config.ts'
+              if vim.fn.filereadable(config_path) == 1 then
+                return config_path
+              end
+            end
+            return vim.fn.getcwd() .. '/jest.config.ts'
+          end,
+          cwd = function(file)
+            if string.find(file, '/packages/') then
+              return string.match(file, '(.-/[^/]+/)src')
+            end
+            return vim.fn.getcwd()
+          end,
+        },
+      },
+      -- Minimal required fields
+      log_level = vim.log.levels.WARN,
+      status = { virtual_text = true },
+      output = { open_on_run = true },
+    }
+
+    vim.keymap.set('n', '<leader>to', function()
+      neotest.output_panel.toggle()
+    end)
+    vim.keymap.set('n', '<leader>tf', function()
+      neotest.run.run(vim.fn.expand '%')
+    end)
+    vim.keymap.set('n', '<leader>tc', function()
+      neotest.run.run()
+    end)
+    vim.keymap.set('n', '<leader>ts', function()
+      neotest.summary.toggle()
+    end)
+
+    -- NEOTEST + DAP
+    vim.keymap.set('n', '<leader>tdc', function()
+      neotest.run.run { strategy = 'dap' }
+    end)
+    vim.keymap.set('n', '<leader>tdf', function()
+      neotest.run.run { vim.fn.expand '%', strategy = 'dap' }
+    end)
+  end,
   opts = function(_, opts)
     opts.adapters = opts.adapters or {}
-    vim.list_extend(opts.adapters, {
-      require 'rustaceanvim.neotest',
-    })
+    table.insert(opts.adapters, require 'rustaceanvim.neotest')
   end,
 }
